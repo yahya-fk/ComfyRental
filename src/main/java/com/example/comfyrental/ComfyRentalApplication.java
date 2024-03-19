da2ed87095bed8ae2aa04c25dcb2df084200787e
yahya-fk <fekyah0@gmail.com>
2024-03-19 22:56:35 +0000

    Initial commit

---
+++ src/main/java/com/example/comfyrental/ComfyRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.comfyrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComfyRentalApplication.class, args);
+    }
+
+}
