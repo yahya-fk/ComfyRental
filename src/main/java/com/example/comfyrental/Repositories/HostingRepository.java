dff9d6739085180cdae2a393b7e6551eee2aaea7
yahya-fk <fekyah0@gmail.com>
2024-03-31 02:34:25 +0000

    Adding Repositories

---
+++ src/main/java/com/example/comfyrental/Repositories/HostingRepository.java
@@ -0,0 +1,10 @@
+package com.example.comfyrental.Repositories;
+
+import com.example.comfyrental.Entities.Hosting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface HostingRepository extends JpaRepository<Hosting,Long> {
+}
