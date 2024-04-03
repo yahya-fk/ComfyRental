695c4617aba0a21a92a34feba7671e3131789340
yahya-fk <fekyah0@gmail.com>
2024-04-03 22:59:55 +0000

    Fix Some issues

---
+++ src/main/java/com/example/comfyrental/Services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.example.comfyrental.Services;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.comfyrental.Entities.User;
+import com.example.comfyrental.Repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService{
+    private UserRepository userRepository;
+    @Override
+    public User saveUser(User user) {
+       /* BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);*/
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        List<User> UserList =findAllUsers();
+        for (User user:UserList) {
+            if(email.equals(user.getEmail())){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteAllUsers() {
+        userRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userRepository.deleteById(id);
+    }
+}
