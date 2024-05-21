70f8501e00e46b7bb70f123f33ff24bdb703d631
Yahya FEKRANE <102445353+yahya-fk@users.noreply.github.com>
2024-05-22 00:52:34 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/comfyrental/Security/SecurityConfig.java
+++ src/main/java/com/example/comfyrental/Security/SecurityConfig.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 @Configuration
 public class SecurityConfig {
     @Bean
+    @Lazy
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
